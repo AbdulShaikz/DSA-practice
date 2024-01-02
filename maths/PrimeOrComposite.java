2f1f2b9fb7b33de86d3fa61c116957f0c18b18b0
Abdul Shaik <starabdul100@gmail.com>
2024-01-02 15:45:51 +0530

    Adding trail division, sieve of eratosthenes approaches to the PrimeOrComposite.java program

--- maths/PrimeOrComposite.java
+++ maths/PrimeOrComposite.java
@@ -8,13 +8,45 @@
             isPrimeOrComposite(n);
         }
 
-        private static void isPrimeOrComposite(int n){
-            for(int i=2;i<=n-1;i++){
+        // TC: O(n)
+        /* private static void isPrimeOrComposite(int n){
+            for(int i=2;i<=n;i++){
                 if(n%i==0){
                     System.out.println(n+" is a Composite number");
                     return;
                 }
             }
             System.out.println(n +" is a Prime number");
+        } */
+
+        //TC: O(sqrt(n))
+        /* private static void isPrimeOrComposite(int n){
+            for(int i=2;i*i<=n;i++){
+                if(n%i==0){
+                    System.out.println(n+" is a Composite number");
+                    return;
+                }
+            }
+            System.out.println(n +" is a Prime number");
+        } */
+
+        //TC: O(n*log(logn)) find all prime numbers up to a given limit
+        private static void isPrimeOrComposite(int n){
+            int[] primes = new int[n];
+            Arrays.fill(primes, 1);
+            for(int i=2;i<n;i++){
+                if(primes[i]==0) {continue;}
+                for(int j=i*i;j<n;j+=i){
+                    primes[j] = 0;  //marking the multiples of i as 0 which means composite
+                }
+            }
+            for(int i=2;i<n;i++){
+                if(primes[i]==0){
+                    System.out.println(i+" is a Composite number");
+                }else{
+                    System.out.println(i +" is a Prime number");
+                }
+            }
         }
+
     }
